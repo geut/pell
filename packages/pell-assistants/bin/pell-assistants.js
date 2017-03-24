773d0bb3a6ef7c59b69e793aae458b0b7d2c341a
Diego Paez <paez.di@gmail.com>
2017-03-24 17:18:11 -0300

    Update: cosmetic

--- packages/pell-assistants/bin/pell-assistants.js
+++ packages/pell-assistants/bin/pell-assistants.js
@@ -5,16 +5,16 @@ var args = process.argv.slice(3);
 
 switch (script) {
 
-case 'eject':
-  var result = spawn.sync(
-    'node',
-    [require.resolve('../scripts/' + script)].concat(args),
-    {stdio: 'inherit'}
-  );
-  process.exit(result.status);
-  break;
-default:
-  console.log('Unknown script "' + script + '".');
-  console.log('Perhaps you need to update pell-assistants?');
-  break;
+    case 'eject':
+        var result = spawn.sync(
+            'node',
+            [require.resolve('../scripts/' + script)].concat(args),
+            {stdio: 'inherit'}
+        );
+        process.exit(result.status);
+        break;
+    default:
+        console.log('Unknown script "' + script + '".');
+        console.log('Perhaps you need to update pell-assistants?');
+        break;
 }
